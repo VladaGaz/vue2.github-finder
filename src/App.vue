0a3a7a8f0c57e8ca5310482d132082f0d0fe7d67
VladaGaz <vladamuzyka7@gmail.com>
2021-06-11 21:04:11 +0300

    git init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrapper">
+    <header>
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-content">
+            <div class="logo">GitHub Finder</div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+
+<style lang="scss">
+.navbar-link {
+  &.router-link-exact-active {
+    color: blue;
+  }
+}
+</style>
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+import Home from '@/pages/Home.vue'
+
+
+export default new Router({
+   routes: [
+      {
+         path: '/',
+         name: "home",
+         component: Home
+      },
+
+   ]
+})
